2176f2166ea5e8f0250238af19fd553540146793
rod <rod@pu-gh.com>
2013-04-30 12:11:09 +0100

    removed uneeded if

--- src/cljs/lowline/functions.cljs
+++ src/cljs/lowline/functions.cljs
@@ -1,17 +1,16 @@
 
-;; ClojureScript implementation of some useful stuff from underscore.js 
+;; ClojureScript implementation of some useful stuff from underscore.js
 ;; that is missing from the standard library.
 
 (ns lowline.functions)
 
-(defn debounce 
+(defn debounce
   ([f] (debounce f 1000))
   ([f timeout]
     (let [id (atom nil)]
       (fn [evt]
-        (if (not (nil? @id))
-          (js/clearTimeout @id))
+        (js/clearTimeout @id)
         (reset! id (js/setTimeout
                    (partial f evt)
                    timeout))))))
- 
+
